ef8a69fda64d2d526e5b0730f7fe7e5e06bd0a9a
sekandar alizada <sekandaralizada92@gmail.com>
2026-02-17 20:58:23 +0430

    writ three project

--- sss.py
+++ sss.py
@@ -1,11 +1,43 @@
-from random import randint,randrange,sample
-x =[]
-for i in range(100):
-    x.append(randrange(1000000000,10000000000,2))
+# from random import randint,randrange,sample
+# x =[]
+# for i in range(100):
+#     x.append(randrange(1000000000,10000000000,2))
+# print(x)
+# y = sample(x,2)
+# print("the winer is:",y)
 
-print(x)
+                 # /first project/
 
-   
+# def user_case (name,age,email):
+#    print("your name is: ",name,"and your age is: ",age, " and your email is: ",email)
+# name = input("enter your name: ")
+# age = int(input("enter your age: "))
+# email = input("enetr your enail: ")
+# user_case(name,age,email)
 
-y = sample(x,2)
-print("the winer is:",y)
+                 # /second porject/
+
+# def opreation(firstNumber,secondNumber):
+#     print("the sum is: ",firstNumber+secondNumber)
+#     print("the munins is: ",firstNumber-secondNumber)
+#     print("the mul is: ",firstNumber*secondNumber)
+#     print("the dev is: ",firstNumber/secondNumber)
+# firstNumber = int(input("inter your first Number: "))
+# secondNumber = int(input("inter your second Number: "))
+# print(opreation(firstNumber,secondNumber))
+
+                 # /thired project/
+
+# list = [1,2,3,4]
+# def opreationOfList(list):
+#     jam = 0
+#     zarb = 1
+#     manf = 0
+#     taqsem=0
+#     for i in list:
+#         jam += i
+#         manf -=i
+#         zarb *=i
+#         taqsem /= i
+#     return jam ,zarb,manf,taqsem
+# print(opreationOfList(list))
